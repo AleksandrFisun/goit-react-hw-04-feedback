2e8d43d5a82bbe51a81a2c8d62cbb26331186469
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2022-10-18 00:18:57 +0300

    1

---
+++ src/components/statistick/Statistick.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StatisticsList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ src/components/wrap/SectionWrap.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Title = styled.h2`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+`;
+export const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+`;
